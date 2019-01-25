fd7a2a7ae411bd8ad82c4b04f2c670ee5e66560e
unknown <juradomartinbenedict@gmail.com>
2019-01-24 21:21:48 -0500

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var db = require("./models")
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
